6b8806b989f25a318df213e4b535a8a849e61323
Calvin Julo <zcglook@hotmail.com>
2023-10-27 01:28:48 +0800

    Update st_main.py

--- st_main.py
+++ st_main.py
@@ -23,8 +23,8 @@ st.write(file_path)
 
 zhouyi = st.checkbox('zhouyi')
 if zhouyi:
-    #subprocess.run(["streamlit", "run", yi_path])
-    subprocess.run(["python", "-m", "streamlit", "run", yi_path])
+    subprocess.run(["streamlit", "run", yi_path,"--server.port", "8501"])
+    #subprocess.run(["python", "-m", "streamlit", "run", yi_path])
     #subprocess.run([f"{sys.executable}", yi_path])
     #subprocess.run(["python", yi_path])
 
