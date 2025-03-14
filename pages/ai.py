55a9613b462551246face474ad10ed2bcea98858
Calvin Julo <zcglook@hotmail.com>
2025-03-15 00:56:11 +0800

    Update ai.py

--- pages/ai.py
+++ pages/ai.py
@@ -31,27 +31,15 @@ def text_to_audio(text,language,topleveldomain):
     
 
 st.write('### Text-to-Speech (TTS) App')
-
-
-
 accents = {
-    "English (Australia)": {"lang": "en", "tld": "com.au"},
     "English (United Kingdom)": {"lang": "en", "tld": "co.uk"},
     "English (United States)": {"lang": "en", "tld": "us"},
-    "English (Canada)": {"lang": "en", "tld": "ca"},
     "English (India)": {"lang": "en", "tld": "co.in"},
-    "English (Ireland)": {"lang": "en", "tld": "ie"},
-    "English (South Africa)": {"lang": "en", "tld": "co.za"},
-    "English (Nigeria)": {"lang": "en", "tld": "com.ng"},
-    "French (Canada)": {"lang": "fr", "tld": "ca"},
     "French (France)": {"lang": "fr", "tld": "fr"},
     "Mandarin (China Mainland)": {"lang": "zh-CN", "tld": "any"},
     "Mandarin (Taiwan)": {"lang": "zh-TW", "tld": "any"},
-    "Portuguese (Brazil)": {"lang": "pt", "tld": "com.br"},
     "Portuguese (Portugal)": {"lang": "pt", "tld": "pt"},
-    "Spanish (Mexico)": {"lang": "es", "tld": "com.mx"},
     "Spanish (Spain)": {"lang": "es", "tld": "es"},
-    "Spanish (United States)": {"lang": "es", "tld": "us"}
 }
 
 text = st.text_area("Enter the text you want to convert to speech:",'')
@@ -59,6 +47,6 @@ text = st.text_area("Enter the text you want to convert to speech:",'')
 accent = st.selectbox("Select language:", list(accents.keys()))
 
 if text:
-    audio_byte = text_to_audio(text,accents[accent][lang],accents[accent][tld])
+    audio_byte = text_to_audio(text,accents[accent]['lang'],accents[accent]['tld'])
     st.audio(audio_byte, format="audio/mp3")
     st.download_button(label="Download Speech", data=audio_byte,file_name="speech.mp3", mime="audio/mp3")
