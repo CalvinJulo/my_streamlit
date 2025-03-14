6274743e761c56a8c26584f5e3e337aa071424ed
Calvin Julo <zcglook@hotmail.com>
2025-03-15 00:43:01 +0800

    Update ai.py

--- pages/ai.py
+++ pages/ai.py
@@ -32,10 +32,19 @@ def text_to_audio(text,language):
 
 st.write('### Text-to-Speech (TTS) App')
 
+accents = {
+    "English (US)": "en",
+    "English (UK)": "en-uk",
+    "English (Australia)": "en-au",
+    "French": "fr",
+    "Spanish": "es"
+}
 
 text = st.text_area("Enter the text you want to convert to speech:",'')
-language = st.selectbox("Select language:", ["en", "es", "fr", "de", "zh-cn"])
+# language = st.selectbox("Select language:", ["en", "es", "fr", "de", "zh-cn"])
+language = st.selectbox("Select language:", list(accents.keys()))
+
 if text:
-    audio_byte = text_to_audio(text,language)
+    audio_byte = text_to_audio(text,accents[language])
     st.audio(audio_byte, format="audio/mp3")
     st.download_button(label="Download Speech", data=audio_byte,file_name="speech.mp3", mime="audio/mp3")
