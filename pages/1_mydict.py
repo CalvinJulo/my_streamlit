9f33e8497ece09387e6450d6fd8665349251d748
Calvin Julo <cividis@hotmail.com>
2025-03-27 03:39:34 +0800

    Update 1_mydict.py

--- pages/1_mydict.py
+++ pages/1_mydict.py
@@ -199,6 +199,7 @@ def parse_wiktionary_by_bs(word):
             current_section=parent[section_name]
             section_stack.append(section_name)
         elif elem.get('class') and elem.get('class')=='NavFrame':
+            st.write(elem)
             Navhead = elem.find_all(class_='NavHead')[0].get_text()
             st.write('kkkk')
             st.write(Navhead)
