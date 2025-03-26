6709cdcc9610750344550321a6533b1a9046bf32
Calvin Julo <cividis@hotmail.com>
2025-03-27 00:06:39 +0800

    Update 1_mydict.py

--- pages/1_mydict.py
+++ pages/1_mydict.py
@@ -204,7 +204,7 @@ def parse_wiktionary_by_bs(word):
         elif elem.name=='ul':
             for li in elem.find_all('li'):
                 audio = li.find_all('a')
-                if audio[1]:
+                if audio[1] and audio[1].get_text()=='file':
                     st.write(audio[1].get_text())
                     text=li.get_text()+'https://en.wiktionary.org'+audio[1].get('href')
                     st.write(text)
