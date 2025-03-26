ede67a8571af789a7dd0c521542906b3e6f4ad95
Calvin Julo <cividis@hotmail.com>
2025-03-26 18:35:36 +0800

    Update 1_mydict.py

--- pages/1_mydict.py
+++ pages/1_mydict.py
@@ -181,13 +181,16 @@ def parse_wiktionary_by_bs(word):
     page = pywikibot.Page(site, word)
     page_html = page.get_parsed_page()
     soup = bs(page_html, 'html.parser')
-    # body = soup.find_all('div',class_=re.compile(r'mw-heading mw-heading'))
-    body = soup.find_all(['div', 'ul','ol','p'])
-    elements = [elem for elem in body if (elem.name == 'div' and elem.get('class_') == re.compile(r'mw-heading mw-heading'))) or elem.name in ['ul', 'p','ol']]
+    body = soup.find_all('div',class_=re.compile(r'mw-heading mw-heading'))
+    elements = soup.find_all(['div', 'ul','ol','p'])
+    for elem in elements:
+        if elem.find_all('div',class_=re.compile(r'mw-heading mw-heading')):
+            st.write(elem.find_all('div',class_=re.compile(r'mw-heading mw-heading'))[0].get_text())
+    # elements = [elem for elem in body if (elem.name == 'div' and elem.get('class_') == re.compile(r'mw-heading mw-heading'))) or elem.name in ['ul', 'p','ol']]
     # body = soup.find_all('div',class_="mw-content-ltr mw-parser-output")[0].find_all()
     # body = soup.find_all('main',id='content',class_='mw-body')[0].get_text()
     # mw-heading mw-heading2
-    return elements
+    return 
 st.write(parse_wiktionary_by_bs(word))
 
 
