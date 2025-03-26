cda91ed2a59b25a98152b543cc611f44096a8731
Calvin Julo <cividis@hotmail.com>
2025-03-27 00:50:57 +0800

    Update 1_mydict.py

--- pages/1_mydict.py
+++ pages/1_mydict.py
@@ -200,7 +200,7 @@ def parse_wiktionary_by_bs(word):
             current_section=parent[section_name]
             section_stack.append(section_name)
         elif elem.name=='p':
-            current_section['intro']=elem.get_text()
+            current_section['intro_']=elem.get_text()
         elif elem.name=='ul':
             for li in elem.find_all('li'):
                 audio = li.find_all('a')
