29063585c5ba314ca551e460beea4bfb5744718f
Calvin Julo <cividis@hotmail.com>
2025-03-26 14:34:19 +0800

    Update 1_mydict.py

--- pages/1_mydict.py
+++ pages/1_mydict.py
@@ -181,8 +181,8 @@ def parse_wiktionary_by_bs(word):
     page = pywikibot.Page(site, word)
     page_html = page.get_parsed_page()
     soup = bs(page_html, 'html.parser')
-    body = soup.find_all('main',class_='mw-body')
-    # body = soup.find_all('mian',class_='mw-body')[0].get_text()
+    body = soup.find_all('main',id='content')
+    # body = soup.find_all('mian',,class_='mw-body')[0].get_text()
     # mw-heading mw-heading2
     return body
 st.write(parse_wiktionary_by_bs(word))
