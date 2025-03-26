65d683fae7f0d9b5d0bb016db860e71ca1e4bb91
Calvin Julo <cividis@hotmail.com>
2025-03-27 01:23:08 +0800

    Update 1_mydict.py

--- pages/1_mydict.py
+++ pages/1_mydict.py
@@ -224,11 +224,6 @@ def parse_wiktionary_by_bs(word):
                 st.write(meanings)
                 current_section.setdefault('meaning', []).append(meanings)
 
-                
-    # elements = [elem for elem in body if (elem.name == 'div' and elem.get('class_') == re.compile(r'mw-heading mw-heading'))) or elem.name in ['ul', 'p','ol']]
-    # body = soup.find_all('div',class_="mw-content-ltr mw-parser-output")[0].find_all()
-    # body = soup.find_all('main',id='content',class_='mw-body')[0].get_text()
-    # mw-heading mw-heading2
     return section_dict
  
 
