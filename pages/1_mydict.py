58845b051b96da5b79abfa00efaf69714810ff61
Calvin Julo <cividis@hotmail.com>
2025-03-27 19:39:50 +0800

    Update 1_mydict.py

--- pages/1_mydict.py
+++ pages/1_mydict.py
@@ -88,7 +88,7 @@ def output_to_streamlit(word):
         st.write('###', pos)
         for syn in synsets:
             st.write(syn['sense_num'],syn['synset_name'])
-            st.write('defintion:','**',syn['definition'],'**',)
+            st.write('defintion:','**'+syn['definition']+'**',)
             st.write('examples:',set(syn['examples']))
             st.write('synonyms:',set(syn['synonyms']),'antonyms:',set(syn['antonyms']),'derivation:',set(syn['derivation']),'pertainyms:',set(syn['pertainyms']))
 
