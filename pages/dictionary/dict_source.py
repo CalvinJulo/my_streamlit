ff2d4936f020c28245ad9f5beb514b4511950e5c
Calvin Julo <cividis@hotmail.com>
2025-07-31 14:31:38 +0800

    Create dict_source.py

---
+++ pages/dictionary/dict_source.py
@@ -0,0 +1,19 @@
+# -*-coding:utf-8 -*-
+
+"""
+# File       : xx.py
+# Time       ：2021/9/11 19:02
+# Author     ：
+# version    ：python 3.9
+# Description： 
+"""
+
+# CMD Run Command ： streamlit run /Users/stock/st_stock.py --server.port 8501
+
+import sys
+import os
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
