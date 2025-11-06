966ddfa0bf8d37e5179ad694f4d91520627dc640
Calvin Julo <cividis@hotmail.com>
2025-11-07 01:08:21 +0800

    Update and rename get_dict.py to my_plotly.py

---
+++ pages/my_plotly.py
@@ -0,0 +1,39 @@
+# -*-coding:utf-8 -*-
+
+"""
+# File       : xx.py
+# Time       ：2021/9/11 19:02
+# Author     ：
+# version    ：
+# Description： 
+"""
+
+# CMD Run Command ： streamlit run /Users/stock/st_stock.py --server.port 8501
+
+import sys
+import os
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
+
+
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+
+
+
+
+
+
+        
+    
+
+
+
+
+
+
