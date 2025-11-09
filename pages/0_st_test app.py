8558e267451d4329358a1dd5b6f2a67a4cf685e8
Calvin Julo <cividis@hotmail.com>
2025-11-09 12:22:16 +0800

    Update 0_st_test app.py

--- pages/0_st_test app.py
+++ pages/0_st_test app.py
@@ -5,3 +5,5 @@ import streamlit as st
 st.write('This is test app one')
 
 st.info('Database \n\n Vocabulary Learning \n\n Analysis & Visualization \n\n AI')
+
+st.write('Database:', 'https://databasesample.streamlit.app/')
