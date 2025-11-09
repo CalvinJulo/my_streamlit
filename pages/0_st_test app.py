211498911573b4e3fc8279318a224b36c03d374b
Calvin Julo <cividis@hotmail.com>
2025-11-09 12:49:55 +0800

    Update 0_st_test app.py

--- pages/0_st_test app.py
+++ pages/0_st_test app.py
@@ -7,3 +7,5 @@ st.write('This is test app one')
 st.info('Database \n\n Vocabulary Learning \n\n Analysis & Visualization \n\n AI')
 
 st.write('Database:', 'https://databasesample.streamlit.app/')
+st.write('Qaunt:', 'https://quantsample.streamlit.app/')
+
